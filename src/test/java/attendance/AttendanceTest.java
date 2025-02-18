ae0fb23e29d3279929df51f99258ab37d02abbfe
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-18 15:08:36 +0900

    refactor(test): 출석부 생성 로직 함수 분리

--- src/test/java/attendance/AttendanceTest.java
+++ src/test/java/attendance/AttendanceTest.java
@@ -18,8 +18,7 @@ public class AttendanceTest {
     // 닉네임과 등교 시간을 입력하면 출석할 수 있다.
     @Test
     void 닉네임과_등교_시간을_입력하면_출석할_수_있다() {
-        AttendanceBook attendanceBook = new AttendanceBook();
-        attendanceBook.attendance(crewName, attendanceTime);
+        AttendanceBook attendanceBook = init();
 
         Set<LocalDateTime> values = attendanceBook.getValues().get(new Crew(crewName));
         assertThat(values).contains(attendanceTime);
@@ -28,8 +27,7 @@ public class AttendanceTest {
     // 출석 후 출석 기록을 확인할 수 있다.
     @Test
     void 출석_후_출석_기록을_확인할_수_있다() {
-        AttendanceBook attendanceBook = new AttendanceBook();
-        attendanceBook.attendance(crewName, attendanceTime);
+        AttendanceBook attendanceBook = init();
 
         Set<LocalDateTime> values = attendanceBook.getValues().get(new Crew(crewName));
         assertThat(values).contains(attendanceTime);
@@ -38,11 +36,16 @@ public class AttendanceTest {
     // 이미 출석한 경우, 다시 출석할 수 없으며 수정 기능을 이용하도록 안내한다.
     @Test
     void 이미_출석한_경우_다시_출석할_수_없으며_수정_기능을_이용하도록_안내한다() {
-        AttendanceBook attendanceBook = new AttendanceBook();
-        attendanceBook.attendance(crewName, attendanceTime);
+        AttendanceBook attendanceBook = init();
 
         assertThatIllegalStateException()
                 .isThrownBy(() -> attendanceBook.attendance(crewName, attendanceTime))
                 .withMessage("금일 출석 기록이 이미 존재합니다.");
+    }
+
+    private AttendanceBook init() {
+        AttendanceBook attendanceBook = new AttendanceBook();
+        attendanceBook.attendance(crewName, attendanceTime);
+        return attendanceBook;
     }
 }
