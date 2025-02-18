e289515a3463cc63ab0cb03d8ea40ba0c59069ba
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-18 22:28:41 +0900

    fix: 정렬 수식 오류 수정

--- src/main/java/attendance/DangerCrewSorter.java
+++ src/main/java/attendance/DangerCrewSorter.java
@@ -3,9 +3,9 @@ package attendance;
 public class DangerCrewSorter {
 
     public int compare(CrewAttendance o1, CrewAttendance o2) {
-        if (o1.getAttendanceStatus().getAbsenceCount() == o2.getAttendanceStatus().getAbsenceCount()) {
+        if (o1.getAttendanceStatus().getRealAbsenceCount() == o2.getAttendanceStatus().getRealAbsenceCount()) {
             if (o1.getAttendanceStatus().getRealLateCount() == o2.getAttendanceStatus().getRealLateCount()) {
-                return o2.getCrew().getName().compareTo(o1.getCrew().getName());
+                return o1.getCrew().getName().compareTo(o2.getCrew().getName());
             }
             return o2.getAttendanceStatus().getRealLateCount() - o1.getAttendanceStatus().getRealLateCount();
         }
