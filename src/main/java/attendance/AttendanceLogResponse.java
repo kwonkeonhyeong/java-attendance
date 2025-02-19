424a16b077f6626dc946b9e233420df19c698456
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-19 17:14:19 +0900

    refactor: 출결 기록 반환 시 출석하지 않은 날은 결석으로 반환하도록 변경

--- src/main/java/attendance/AttendanceLogResponse.java
+++ src/main/java/attendance/AttendanceLogResponse.java
@@ -1,30 +1,49 @@
 package attendance;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class AttendanceLogResponse {
 
     private final String crewName;
-    private final List<LocalDateTime> timeLogs;
+    private final List<TimeLogResponse> timeLogs;
 
-    private AttendanceLogResponse(String crewName, List<LocalDateTime> timeLogs) {
+    private AttendanceLogResponse(String crewName, List<TimeLogResponse> timeLogs) {
         this.crewName = crewName;
         this.timeLogs = timeLogs;
     }
 
-    public static AttendanceLogResponse of(Crew crew, List<LocalDateTime> timeLogs) {
-        return new AttendanceLogResponse(
-                crew.getName(),
-                timeLogs
-        );
+    public static AttendanceLogResponse of(Crew crew, List<LocalDateTime> timeLogs,
+                                           AttendanceAnalyzer attendanceAnalyzer) {
+        List<LocalDate> dateLogs = timeLogs.stream()
+                .map(LocalDateTime::toLocalDate)
+                .toList();
+
+        List<LocalDate> notExistsDatesBeforeToday = Calender.getNotExistsDatesBeforeToday(dateLogs);
+
+        List<TimeLogResponse> existTimeLogs = timeLogs.stream()
+                .map(timeLog -> TimeLogResponse.of(timeLog, attendanceAnalyzer))
+                .toList();
+
+        List<TimeLogResponse> notExistTimeLogs = notExistsDatesBeforeToday.stream()
+                .map(TimeLogResponse::of)
+                .toList();
+
+        List<TimeLogResponse> timeLogResponses = Stream.concat(existTimeLogs.stream(), notExistTimeLogs.stream())
+                .sorted(Comparator.comparing(TimeLogResponse::getDate))
+                .toList();
+
+        return new AttendanceLogResponse(crew.getName(), timeLogResponses);
     }
 
     public String getCrewName() {
         return crewName;
     }
 
-    public List<LocalDateTime> getTimeLogs() {
+    public List<TimeLogResponse> getTimeLogs() {
         return timeLogs;
     }
 }
