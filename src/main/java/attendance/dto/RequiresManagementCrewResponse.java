feb673c4856ad30a70fce9531ec2921b6a3f86a4
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-20 15:14:01 +0900

    feat: 관리 대상 크루 응답 DTO 생성

---
+++ src/main/java/attendance/dto/RequiresManagementCrewResponse.java
@@ -0,0 +1,52 @@
+package attendance.dto;
+
+import attendance.model.CrewAttendanceStatus;
+
+public class RequiresManagementCrewResponse {
+    private final String crewName;
+    private final int absenceCount;
+    private final int lateCount;
+    private final String managementStatus;
+
+    private RequiresManagementCrewResponse(String crewName, int absenceCount, int lateCount, String managementStatus) {
+        this.crewName = crewName;
+        this.absenceCount = absenceCount;
+        this.lateCount = lateCount;
+        this.managementStatus = managementStatus;
+    }
+
+    public static RequiresManagementCrewResponse from(CrewAttendanceStatus crewAttendanceStatus) {
+        return new RequiresManagementCrewResponse(
+                crewAttendanceStatus.getCrew().getName(),
+                crewAttendanceStatus.getAbsenceCount(),
+                crewAttendanceStatus.getLateCount(),
+                crewAttendanceStatus.getManagementStatus().getName()
+        );
+    }
+
+    public String getCrewName() {
+        return crewName;
+    }
+
+    public int getAbsenceCount() {
+        return absenceCount;
+    }
+
+    public int getLateCount() {
+        return lateCount;
+    }
+
+    public String getManagementStatus() {
+        return managementStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "DangerCrewResponse{" +
+                "crewName='" + crewName + '\'' +
+                ", absenceCount=" + absenceCount +
+                ", lateCount=" + lateCount +
+                ", managementStatus='" + managementStatus + '\'' +
+                '}' + '\n';
+    }
+}
