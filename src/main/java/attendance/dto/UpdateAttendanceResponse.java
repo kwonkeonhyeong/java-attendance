acb7b1587139742fde347706057988f5334bb749
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-20 17:33:31 +0900

    feat: before, after status 추가

--- src/main/java/attendance/dto/UpdateAttendanceResponse.java
+++ src/main/java/attendance/dto/UpdateAttendanceResponse.java
@@ -1,5 +1,6 @@
 package attendance.dto;
 
+import attendance.model.AttendanceStatus;
 import java.time.LocalDateTime;
 
 public class UpdateAttendanceResponse {
@@ -7,9 +8,20 @@ public class UpdateAttendanceResponse {
     private final LocalDateTime before;
     private final LocalDateTime after;
 
-    public UpdateAttendanceResponse(LocalDateTime before, LocalDateTime after) {
+    private final String beforeStatus;
+    private final String afterStatus;
+
+    private UpdateAttendanceResponse(LocalDateTime before, LocalDateTime after, String beforeStatus,
+                                     String afterStatus) {
         this.before = before;
         this.after = after;
+        this.beforeStatus = beforeStatus;
+        this.afterStatus = afterStatus;
+    }
+
+    public static UpdateAttendanceResponse of(LocalDateTime before, LocalDateTime after, AttendanceStatus beforeStatus,
+                                              AttendanceStatus afterStatus) {
+        return new UpdateAttendanceResponse(before, after, beforeStatus.getName(), afterStatus.getName());
     }
 
     public LocalDateTime getBefore() {
@@ -18,5 +30,13 @@ public class UpdateAttendanceResponse {
 
     public LocalDateTime getAfter() {
         return after;
+    }
+
+    public String getBeforeStatus() {
+        return beforeStatus;
+    }
+
+    public String getAfterStatus() {
+        return afterStatus;
     }
 }
