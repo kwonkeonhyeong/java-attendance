562a77b3be0e3bb0c7a174ef21a9a50fe0a9bcd2
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-20 17:32:47 +0900

    feat: managementStatus 추가

--- src/main/java/attendance/dto/CrewAttendanceLogResponse.java
+++ src/main/java/attendance/dto/CrewAttendanceLogResponse.java
@@ -1,20 +1,29 @@
 package attendance.dto;
 
+import attendance.model.AttendanceStatus;
+import attendance.model.ManagementStatus;
 import attendance.model.domain.crew.Crew;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CrewAttendanceLogResponse {
 
     private final String crewName;
     private final List<AttendanceLogResponse> timeLogs;
+    private final String managementStatus;
 
-    private CrewAttendanceLogResponse(String crewName, List<AttendanceLogResponse> timeLogs) {
+    private CrewAttendanceLogResponse(String crewName, List<AttendanceLogResponse> timeLogs, String managementStatus) {
         this.crewName = crewName;
         this.timeLogs = timeLogs;
+        this.managementStatus = managementStatus;
     }
 
-    public static CrewAttendanceLogResponse of(Crew crew, List<AttendanceLogResponse> attendanceLogRespons) {
-        return new CrewAttendanceLogResponse(crew.getName(), attendanceLogRespons);
+    public static CrewAttendanceLogResponse of(Crew crew, List<AttendanceLogResponse> attendanceLogResponse,
+                                               ManagementStatus managementStatus) {
+        return new CrewAttendanceLogResponse(crew.getName(), attendanceLogResponse, managementStatus.getName());
     }
 
     public String getCrewName() {
@@ -23,5 +32,20 @@ public class CrewAttendanceLogResponse {
 
     public List<AttendanceLogResponse> getTimeLogs() {
         return timeLogs;
+    }
+
+    public String getManagementStatus() {
+        return managementStatus;
+    }
+
+    public Map<String, Integer> getAttendanceStatusStatistics() {
+        return Arrays.stream(AttendanceStatus.values())
+                .collect(Collectors.toMap(
+                        AttendanceStatus::getName,
+                        status -> (int) timeLogs.stream()
+                                .filter(timeLog -> AttendanceStatus.from(
+                                        timeLog.getAttendanceStatusResponse().getAttendanceStatus()) == status)
+                                .count(),
+                        (e1, e2) -> e1, LinkedHashMap::new));
     }
 }
