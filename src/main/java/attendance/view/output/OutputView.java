8f569968408b7480d2da111e1e8284249ebc4d5f
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-20 17:28:31 +0900

    feat: 출력 뷰 구현

---
+++ src/main/java/attendance/view/output/OutputView.java
@@ -0,0 +1,52 @@
+package attendance.view.output;
+
+import attendance.dto.AttendanceLogResponse;
+import attendance.dto.CrewAttendanceLogResponse;
+import attendance.dto.UpdateAttendanceResponse;
+import attendance.view.input.KoreaDayOfWeek;
+import java.time.LocalTime;
+import java.util.Map;
+
+public class OutputView {
+
+    public void printAttendanceLog(AttendanceLogResponse response) {
+        String message = String.format("%d월 %d일 %s %s (%s)",
+                response.getDate().getMonthValue(),
+                response.getDate().getDayOfMonth(),
+                KoreaDayOfWeek.from(response.getDate().getDayOfWeek()).getName(),
+                formatTime(response.getTime()),
+                response.getAttendanceStatusResponse().getAttendanceStatus());
+        System.out.println(message);
+    }
+
+    public void printUpdateAttendanceResponse(UpdateAttendanceResponse updateAttendanceResponse) {
+        String message = String.format("%d월 %d일 %s %s (%s) -> %s (%s) 수정 완료!",
+                updateAttendanceResponse.getBefore().getMonthValue(),
+                updateAttendanceResponse.getBefore().getDayOfMonth(),
+                KoreaDayOfWeek.from(updateAttendanceResponse.getBefore().getDayOfWeek()).getName(),
+                formatTime(updateAttendanceResponse.getBefore().toLocalTime()),
+                updateAttendanceResponse.getBeforeStatus(),
+                formatTime(updateAttendanceResponse.getAfter().toLocalTime()),
+                updateAttendanceResponse.getAfterStatus());
+        System.out.println(message);
+    }
+
+    public void printCrewAttendanceLogResponse(CrewAttendanceLogResponse crewAttendanceLogResponse) {
+        System.out.printf("이번 달 %s의 출석 기록입니다.%n%n", crewAttendanceLogResponse.getCrewName());
+
+        crewAttendanceLogResponse.getTimeLogs().forEach(this::printAttendanceLog);
+
+        Map<String, Integer> attendanceStatusStatistics = crewAttendanceLogResponse.getAttendanceStatusStatistics();
+        System.out.println();
+        attendanceStatusStatistics.forEach((status, count) -> System.out.printf("%s: %d회%n", status, count));
+
+        System.out.printf("%n%s 대상자입니다.%n", crewAttendanceLogResponse.getManagementStatus());
+    }
+
+    public String formatTime(LocalTime time) {
+        if (time == null) {
+            return "--:--";
+        }
+        return String.format("%2d:%2d", time.getHour(), time.getMinute());
+    }
+}
