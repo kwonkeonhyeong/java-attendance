eecf294f992406c3b72f2531b2d8add9fb46a2ff
KimDaehyeon <daehyeon3351@gmail.com>
2025-02-20 17:34:20 +0900

    feat: Controller 를 사용하도록 구현

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -1,7 +1,12 @@
 package attendance;
 
+import attendance.controller.Controller;
+
 public class Application {
 
     public static void main(String[] args) {
+
+        Controller controller = new Controller();
+        controller.run();
     }
 }
