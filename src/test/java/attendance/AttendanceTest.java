508536ba65e136491682458217eab9315dbab624
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-21 15:54:00 +0900

    refactor(test): 테스트 데이터 적용 로직 변경에 따른 테스트 리펙터링

--- src/test/java/attendance/AttendanceTest.java
+++ src/test/java/attendance/AttendanceTest.java
@@ -1,175 +1,139 @@
 package attendance;
 
-import static java.time.DayOfWeek.MONDAY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import attendance.dto.AttendanceLogResponse;
 import attendance.dto.CrewAttendanceLogResponse;
-import attendance.dto.RequiresManagementCrewResponse;
 import attendance.dto.UpdateAttendanceResponse;
-import attendance.model.Calender;
 import attendance.model.domain.crew.Crew;
-import attendance.model.domain.crew.CrewAttendanceComparator;
-import attendance.model.domain.crew.DefaultCrewAttendanceComparator;
 import attendance.model.repository.AttendanceRepository;
 import attendance.model.repository.CrewAttendanceDeserializer;
 import attendance.model.service.AttendanceService;
 import java.nio.file.Path;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.AbstractMap.SimpleImmutableEntry;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.Objects;
-import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 public class AttendanceTest {
 
-    private static final CrewAttendanceComparator crewAttendanceComparator = new DefaultCrewAttendanceComparator();
-    private static final String crewName = "pobi";
-    private static final LocalDateTime attendanceTime =
-            LocalDateTime.of(LocalDate.of(2024, 12, 14), LocalTime.of(13, 20));
-    private final CrewAttendanceDeserializer crewAttendanceDeserializer = new CrewAttendanceDeserializer();
-    private final Path crewAttendanceDataPath = Path.of("src/main/resources/attendances.csv");
+  private AttendanceRepository attendanceRepository;
+  private AttendanceService attendanceService;
 
-    static Stream<Arguments> 닉네임을_통해_전날까지의_크루_출석_기록_확인_테스트_케이스() {
-        return Stream.of(
-                Arguments.of("pobi", List.of(
-                                LocalDateTime.of(LocalDate.of(2024, 12, 10), LocalTime.of(13, 20)),
-                                LocalDateTime.of(LocalDate.of(2024, 12, 13), LocalTime.of(15, 20)),
-                                LocalDateTime.of(LocalDate.of(2024, 12, 12), LocalTime.of(15, 20)))
-                        , 3, 7)
-        );
-    }
+  @BeforeEach
+  void beforeEach() {
+    final CrewAttendanceDeserializer crewAttendanceDeserializer = new CrewAttendanceDeserializer();
+    final Path crewAttendanceDataPath = Path.of("src/main/resources/attendances_test.csv");
+    attendanceRepository = new AttendanceRepository(crewAttendanceDeserializer,
+        crewAttendanceDataPath);
+    attendanceService = new AttendanceService(attendanceRepository);
+  }
+  // 닉네임과 등교 시간을 입력하면 출석할 수 있다.
 
-    static Stream<Arguments> 전날까지의_크루_출석_기록을_통해_제적_위험자_정렬해서_반환_테스트() {
-        return Stream.of(
-                Arguments.of(
-                        List.of(
-                                new AttendanceData("pobi", getAbsenceTimes(0, 1), (getLateTimes(1, 5))),
-                                new AttendanceData("anni", getAbsenceTimes(0, 2), (getLateTimes(2, 4)))
-                        ),
-                        List.of(new SimpleImmutableEntry<>("anni", "경고"), new SimpleImmutableEntry<>("pobi", "경고"))
-                ),
-                Arguments.of(
-                        List.of(
-                                new AttendanceData("abd", getAbsenceTimes(0, 4), (getLateTimes(4, 5))),
-                                new AttendanceData("abc", getAbsenceTimes(0, 4), (getLateTimes(4, 5)))
-                        ),
-                        List.of(new SimpleImmutableEntry<>("abc", "면담"), new SimpleImmutableEntry<>("abd", "면담"))
-                ),
-                Arguments.of(
-                        List.of(
-                                new AttendanceData("pobi", getAbsenceTimes(0, 5), (getLateTimes(5, 8))),
-                                new AttendanceData("anni", getAbsenceTimes(0, 6), (getLateTimes(6, 6)))
-                        ),
-                        List.of(new SimpleImmutableEntry<>("anni", "제적"), new SimpleImmutableEntry<>("pobi", "제적"))
-                )
-        );
-    }
+  @Test
+  void 닉네임과_등교_시간을_입력하면_출석할_수_있다() {
 
-    static List<LocalDateTime> getAbsenceTimes(int start, int end) {
-        return Calender.WEEKDAY.getDays().stream()
-                .filter(day -> day.getDayOfWeek() != MONDAY)
-                .map(day -> LocalDateTime.of(day, LocalTime.of(10, 32)))
-                .toList()
-                .subList(start, end);
-    }
+    Crew crew = Crew.from("이든");
 
-    static List<LocalDateTime> getLateTimes(int start, int end) {
-        return Calender.WEEKDAY.getDays().stream()
-                .filter(day -> day.getDayOfWeek() != MONDAY)
-                .map(day -> LocalDateTime.of(day, LocalTime.of(10, 7)))
-                .toList()
-                .subList(start, end);
-    }
+    LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 14, 10, 3);
 
-    // 닉네임과 등교 시간을 입력하면 출석할 수 있다.
-    @Test
-    void 닉네임과_등교_시간을_입력하면_출석할_수_있다() {
-        AttendanceRepository attendanceRepository = init();
+    attendanceService.attendance(crew, localDateTime);
 
-        List<LocalDateTime> values = attendanceRepository.findByCrew(Crew.from(crewName));
-        assertThat(values).contains(attendanceTime);
-    }
+    List<LocalDateTime> values = attendanceRepository.findByCrew(crew);
 
-    // 출석 후 출석 기록을 확인할 수 있다.
-    @Test
-    void 출석_후_출석_기록을_확인할_수_있다() {
-        AttendanceRepository attendanceRepository = init();
+    assertThat(values).contains(localDateTime);
+  }
 
-        List<LocalDateTime> values = attendanceRepository.findByCrew(Crew.from(crewName));
-        assertThat(values).contains(attendanceTime);
-    }
+  // 출석 기록을 확인할 수 있다.
+  @Test
+  void 출석_기록을_확인할_수_있다() {
 
-    // 이미 출석한 경우, 다시 출석할 수 없으며 수정 기능을 이용하도록 안내한다.
-    @Test
-    void 이미_출석한_경우_다시_출석할_수_없으며_수정_기능을_이용하도록_안내한다() {
-        AttendanceRepository attendanceRepository = init();
+    String crewName = "이든";
 
-        assertThatIllegalStateException()
-                .isThrownBy(() -> attendanceRepository.save(Crew.from(crewName), attendanceTime))
-                .withMessage("금일 출석 기록이 이미 존재합니다.");
-    }
+    Crew crew = Crew.from(crewName);
 
-    // 출석 확인을 수정하려면 닉네임, 수정하려는 날짜, 등교 시간을 입력하여 기록을 수정할 수 있다.
-    @Test
-    void 출석을_수정할_수_있다() {
-        AttendanceRepository attendanceRepository = init();
-        AttendanceService service = new AttendanceService(attendanceRepository);
+    CrewAttendanceLogResponse attendanceLog = attendanceService.getAttendanceLog(crew);
 
-        LocalDateTime timeToEdit = LocalDateTime.of(LocalDate.of(2024, 12, 14), LocalTime.of(14, 20));
+    List<AttendanceLogResponse> searchTimeLogs = attendanceLog.getTimeLogs();
 
-        service.updateAttendance(Crew.from(crewName), timeToEdit);
+    String searchCrewName = attendanceLog.getCrewName();
 
-        List<LocalDateTime> values = attendanceRepository.findByCrew(Crew.from(crewName));
+    assertThat(crewName).isEqualTo(searchCrewName);
+    assertThat(searchTimeLogs).isNotEmpty();
+  }
 
-        assertThat(values).contains(timeToEdit);
-        assertThat(values).hasSize(1);
-    }
+  // 이미 출석한 경우, 다시 출석할 수 없으며 수정 기능을 이용하도록 안내한다.
+  @Test
+  void 이미_출석한_경우_다시_출석할_수_없으며_수정_기능을_이용하도록_안내한다() {
 
-    // 수정 후에는 변경 전과 변경 후의 출석 기록을 확인할 수 있다.
-    @Test
-    void 수정_후에는_변경_전과_변경_후의_출석_기록을_확인할_수_있다() {
-        AttendanceRepository attendanceRepository = init();
-        AttendanceService service = new AttendanceService(attendanceRepository);
+    Crew crew = Crew.from("이든");
 
-        LocalDateTime timeToEdit = LocalDateTime.of(LocalDate.of(2024, 12, 14), LocalTime.of(14, 20));
+    LocalDateTime localDateTime = LocalDateTime.of(2024, 12, 14, 10, 3);
 
-        UpdateAttendanceResponse updateAttendanceResponse = service.updateAttendance(Crew.from(crewName), timeToEdit);
+    attendanceService.attendance(crew, localDateTime);
 
-        assertThat(updateAttendanceResponse.getBefore()).isEqualTo(attendanceTime);
-        assertThat(updateAttendanceResponse.getAfter()).isEqualTo(timeToEdit);
-    }
+    assertThatIllegalStateException()
+        .isThrownBy(() -> attendanceService.attendance(crew, localDateTime))
+        .withMessage("금일 출석 기록이 이미 존재합니다.");
+  }
 
-    // 닉네임을 입력하면 전날까지의 크루 출석 기록을 확인할 수 있다.
-    @ParameterizedTest
-    @MethodSource("닉네임을_통해_전날까지의_크루_출석_기록_확인_테스트_케이스")
-    void 닉네임을_통해_전날까지의_크루_출석_기록_확인(String crewName, List<LocalDateTime> times, int existCount, int nonExistCount) {
-        AttendanceRepository attendanceRepository = new AttendanceRepository(crewAttendanceDeserializer,
-                crewAttendanceDataPath);
-        AttendanceService attendanceService = new AttendanceService(attendanceRepository);
-        times.forEach(time -> attendanceRepository.save(Crew.from(crewName), time));
+  // 출석 확인을 수정하려면 닉네임, 수정하려는 날짜, 등교 시간을 입력하여 기록을 수정할 수 있다.
+  @Test
+  void 출석을_수정할_수_있다() {
 
-        CrewAttendanceLogResponse attendanceLog = attendanceService.getAttendanceLog(Crew.from(crewName));
+    Crew crew = Crew.from("이든");
 
-        assertAll(
-                () -> assertThat(attendanceLog.getCrewName()).isEqualTo(crewName),
-                () -> assertThat(attendanceLog.getTimeLogs().stream().map(AttendanceLogResponse::getDate)).isSorted(),
-                () -> assertThat(attendanceLog.getTimeLogs().stream().map(AttendanceLogResponse::getTime)
-                        .filter(Objects::isNull).count()).isEqualTo(nonExistCount),
-                () -> assertThat(attendanceLog.getTimeLogs().stream().map(AttendanceLogResponse::getTime)
-                        .filter(Objects::nonNull).count()).isEqualTo(existCount)
-        );
-    }
+    LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 14, 14, 20);
+
+    attendanceService.attendance(crew, attendanceTime);
+
+    LocalDateTime timeToEdit = LocalDateTime.of(2024, 12, 14, 14, 20);
+
+    attendanceService.updateAttendance(crew, timeToEdit);
+
+    List<LocalDateTime> values = attendanceRepository.findByCrew(crew);
+
+    assertThat(values).contains(timeToEdit);
+  }
+
+  // 수정 후에는 변경 전과 변경 후의 출석 기록을 확인할 수 있다.
+  @Test
+  void 수정_후에는_변경_전과_변경_후의_출석_기록을_확인할_수_있다() {
+
+    Crew crew = Crew.from("이든");
+
+    LocalDateTime attendanceTime = LocalDateTime.of(2024, 12, 14, 10, 20);
+
+    attendanceService.attendance(crew, attendanceTime);
+
+    LocalDateTime timeToEdit = LocalDateTime.of(2024, 12, 14, 10, 4);
+
+    UpdateAttendanceResponse updateAttendanceResponse = attendanceService.updateAttendance(crew,
+        timeToEdit);
+
+    assertThat(updateAttendanceResponse.getBefore()).isEqualTo(attendanceTime);
+    assertThat(updateAttendanceResponse.getAfter()).isEqualTo(timeToEdit);
+  }
+
+  /*
+  // 닉네임을 입력하여 크루 출석 기록을 확인할 수 있다.
+  void 닉네임을_입력하여_크루_출석_기록_확인(String crewName, List<LocalDateTime> times, int existCount,
+      int nonExistCount) {
+
+    CrewAttendanceLogResponse attendanceLog = attendanceService.getAttendanceLog(
+        Crew.from(crewName));
+
+    assertAll(
+        () -> assertThat(attendanceLog.getCrewName()).isEqualTo(crewName),
+        () -> assertThat(
+            attendanceLog.getTimeLogs().stream().map(AttendanceLogResponse::getDate)).isSorted(),
+        () -> assertThat(attendanceLog.getTimeLogs().stream().map(AttendanceLogResponse::getTime)
+            .filter(Objects::isNull).count()).isEqualTo(nonExistCount),
+        () -> assertThat(attendanceLog.getTimeLogs().stream().map(AttendanceLogResponse::getTime)
+            .filter(Objects::nonNull).count()).isEqualTo(existCount)
+    );
+  }
 
     // 전날까지의 크루 출석 기록을 바탕으로 제적 위험자를 파악한다.
     // 제적 위험자는 제적 대상자, 면담 대상자, 경고 대상자순으로 출력하며, 대상 항목별 정렬 순서는 지각을 결석으로 간주하여 내림차순한다.
@@ -203,13 +167,6 @@ public class AttendanceTest {
 
     }
 
-    private AttendanceRepository init() {
-        AttendanceRepository attendanceRepository = new AttendanceRepository(crewAttendanceDeserializer,
-                crewAttendanceDataPath);
-        attendanceRepository.save(Crew.from(crewName), attendanceTime);
-        return attendanceRepository;
-    }
-
     private static class AttendanceData {
         private final String crewName;
         private final List<LocalDateTime> times;
@@ -219,5 +176,5 @@ public class AttendanceTest {
             this.times = new LinkedList<>(absenceTimes);
             this.times.addAll(lateTimes);
         }
-    }
-}
+    }*/
+  }
