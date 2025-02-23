9a65a4e66683f60f17216c40331ad6f42cae0fc5
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-23 11:49:47 +0900

    fix: TimeLogsTest 이름 변경
    - TimeLogsTest -> TimeLogTest 로 변경

--- src/test/java/attendance/model/domain/log/TimeLogTest.java
+++ src/test/java/attendance/model/domain/log/TimeLogTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class TimeLogsTest {
+class TimeLogTest {
 
   // TimeLogs, TimeLog
   @Test
