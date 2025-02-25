105949865ff01ebcab9d8066ad9a0f535c45fec7
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-25 22:27:13 +0900

    refactor: @FunctionalInterface 제거

--- src/main/java/attendance/model/domain/crew/comprator/CrewAttendanceComparator.java
+++ src/main/java/attendance/model/domain/crew/comprator/CrewAttendanceComparator.java
@@ -3,6 +3,5 @@ package attendance.model.domain.crew.comprator;
 import attendance.model.domain.crew.CrewAttendanceStatus;
 import java.util.Comparator;
 
-@FunctionalInterface
 public interface CrewAttendanceComparator extends Comparator<CrewAttendanceStatus> {
 }
