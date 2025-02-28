53cf571a3a9c1c0cea47b546f99f2b253a7b59d9
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-28 19:22:54 +0900

    feat(green): Crew 생성 기능 구현
    - 크루 이름이 2~4자가 아닌 경우 예외 발생 기능 구현

--- src/main/java/attendance/Crew.java
+++ src/main/java/attendance/Crew.java
@@ -2,6 +2,10 @@ package attendance;
 
 public class Crew {
 
-  public Crew(String s) {
+  public Crew(String name) {
+    if (name.length() < 2 || name.length() > 4) {
+      throw new IllegalArgumentException();
+    }
   }
+
 }
