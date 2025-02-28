6ac5648860b39a1c7b4318f04b8ee888daab09f9
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-28 20:08:22 +0900

    refactor: TimeLog validate 메서드 분리

--- src/main/java/attendance/TimeLog.java
+++ src/main/java/attendance/TimeLog.java
@@ -1,13 +1,27 @@
 package attendance;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class TimeLog {
 
+  private static final LocalTime CAMPUS_OPEN_TIME = LocalTime.of(8, 0);
+  private static final LocalTime CAMPUS_CLOSE_TIME = LocalTime.of(23, 0);
+  private static final String CAMPUS_CLOSED_MESSAGE = "캠퍼스 운영 시간이 아닙니다";
+
+  private final LocalDate date;
+  private final LocalTime time;
+
   public TimeLog(LocalDateTime dateTime) {
-    if(dateTime.toLocalTime().isBefore(LocalTime.of(8,0)) || dateTime.toLocalTime().isAfter(LocalTime.of(23,0))) {
-      throw new IllegalArgumentException("캠퍼스 운영 시간이 아닙니다");
+    validateCampusOperatingTime(dateTime.toLocalTime());
+    this.date = dateTime.toLocalDate();
+    this.time = dateTime.toLocalTime();
+  }
+
+  private static void validateCampusOperatingTime(LocalTime time) {
+    if(time.isBefore(CAMPUS_OPEN_TIME) || time.isAfter(CAMPUS_CLOSE_TIME)) {
+      throw new IllegalArgumentException(CAMPUS_CLOSED_MESSAGE);
     }
   }
 }
