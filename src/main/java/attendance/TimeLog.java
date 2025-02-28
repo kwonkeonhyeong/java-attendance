9f9c87d5417acb0f5ca3941dfde84c1c801d04a1
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-28 19:57:34 +0900

    feat(green): TimeLog 생성 기능 구현

--- src/main/java/attendance/TimeLog.java
+++ src/main/java/attendance/TimeLog.java
@@ -1,9 +1,13 @@
 package attendance;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class TimeLog {
 
   public TimeLog(LocalDateTime dateTime) {
+    if(dateTime.toLocalTime().isBefore(LocalTime.of(8,0)) || dateTime.toLocalTime().isAfter(LocalTime.of(23,0))) {
+      throw new IllegalArgumentException("캠퍼스 운영 시간이 아닙니다");
+    }
   }
 }
