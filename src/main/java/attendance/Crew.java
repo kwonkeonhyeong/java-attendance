92ebe52e0a3fd9bd89f7dd9bd2b5f09186c93c94
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-28 19:32:28 +0900

    refactor: Crew validate 메서드 분리

--- src/main/java/attendance/Crew.java
+++ src/main/java/attendance/Crew.java
@@ -2,7 +2,14 @@ package attendance;
 
 public class Crew {
 
+  private final String name;
+
   public Crew(String name) {
+    validateNameLength(name);
+    this.name = name;
+  }
+
+  private void validateNameLength(String name) {
     if (name.length() < 2 || name.length() > 4) {
       throw new IllegalArgumentException();
     }
