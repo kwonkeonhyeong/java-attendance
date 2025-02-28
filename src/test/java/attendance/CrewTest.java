1544d135969d86f0546f640908fe548c9b4a423a
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-02-28 19:35:33 +0900

    test(refactor): 크루 닉네임 글자수 관련 테스트 변경

--- src/test/java/attendance/CrewTest.java
+++ src/test/java/attendance/CrewTest.java
@@ -3,16 +3,24 @@ package attendance;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CrewTest {
-  @Test
-  void 크루_이름은_2_4글자_만_이용_가능() {
-    assertThatCode(() -> new Crew("1234")).doesNotThrowAnyException();
+
+  @DisplayName("크루_닉네임은_2_4글자_만_이용_가능")
+  @ParameterizedTest
+  @ValueSource(strings = {"12","123","1234"})
+  void crewNicknameLengthTest1(String name) {
+    assertThatCode(() -> new Crew(name)).doesNotThrowAnyException();
   }
 
-  @Test
-  void 크루_이름이_2_4자가_아닌_경우_예외_발생() {
-    Assertions.assertThatThrownBy(() -> new Crew("12345"));
+  @DisplayName("크루_닉네임이_2_4자가_아닌_경우_예외_발생")
+  @ParameterizedTest
+  @ValueSource(strings = {"12","123","1234"})
+  void crewNicknameLengthTest2(String name) {
+    Assertions.assertThatThrownBy(() -> new Crew(name));
   }
+
 }
