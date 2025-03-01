89d40f3b233ada5a75739876ca50195ff22a83cf
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-01 23:08:10 +0900

    test(red): 동일한 AttendanceRecord 등록 시 예외 발생 테스트 추가

--- src/test/java/attendance/AttendanceRecordsTest.java
+++ src/test/java/attendance/AttendanceRecordsTest.java
@@ -1,17 +1,33 @@
 package attendance;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.time.LocalDateTime;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AttendanceRecordsTest {
+
+  private final AttendanceRecords attendanceRecords = new AttendanceRecords();
+
   @DisplayName("출석_기록_저장_후_저장된_기록_반환")
   @Test
   void saveAttendanceRecordTest() {
-    AttendanceRecords attendanceRecords = new AttendanceRecords();
-    AttendanceRecord attendanceRecord = new AttendanceRecord("히포", LocalDateTime.of(2025, 2, 28, 10, 0));
+    AttendanceRecord attendanceRecord = new AttendanceRecord("히포",
+        LocalDateTime.of(2025, 2, 28, 10, 0));
     AttendanceRecord savedAttendanceRecord = attendanceRecords.save(attendanceRecord);
     Assertions.assertThat(savedAttendanceRecord).isEqualTo(attendanceRecord);
+  }
+
+  @DisplayName("동일한_출석_기록이_존재하는_경우_예외_발생")
+  @Test
+  void validateExistsAttendanceRecordTest() {
+    AttendanceRecord attendanceRecord = new AttendanceRecord("히포",
+        LocalDateTime.of(2025, 2, 28, 10, 0));
+    attendanceRecords.save(attendanceRecord);
+    assertThatThrownBy(() -> attendanceRecords.save(attendanceRecord))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("해당 일 출석 기록이 이미 존재합니다");
   }
 }
