bbffcedc5ed85c13dc09f934ab64ddd092464442
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-01 23:17:37 +0900

    feat(green): 동일한 출석 기록이 존재하는 경우 예외 발생 기능 구현

--- src/main/java/attendance/AttendanceRecords.java
+++ src/main/java/attendance/AttendanceRecords.java
@@ -8,6 +8,9 @@ public class AttendanceRecords {
   private final List<AttendanceRecord> attendanceRecords = new ArrayList<>();
 
   public AttendanceRecord save(AttendanceRecord attendanceRecord) {
+    if(attendanceRecords.contains(attendanceRecord)) {
+      throw new IllegalArgumentException("해당 일 출석 기록이 이미 존재합니다");
+    }
     attendanceRecords.add(attendanceRecord);
     return attendanceRecord;
   }
