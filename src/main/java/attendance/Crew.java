9087083d14a6a5f08422e80727e9fe72b553b4e0
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-01 11:20:37 +0900

    feat(green): Crew equals 구현

--- src/main/java/attendance/Crew.java
+++ src/main/java/attendance/Crew.java
@@ -1,5 +1,7 @@
 package attendance;
 
+import java.util.Objects;
+
 public class Crew {
 
   private final String name;
@@ -13,6 +15,20 @@ public class Crew {
     if (name.length() < 2 || name.length() > 4) {
       throw new IllegalArgumentException();
     }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Crew crew = (Crew) o;
+    return Objects.equals(name, crew.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(name);
   }
 
 }
