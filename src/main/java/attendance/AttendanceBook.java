28aee61dbac59d31c96640e75317d2726d152703
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-02 00:18:45 +0900

    feat(green): AttendanceBook 출석 확인 기능 구현

--- src/main/java/attendance/AttendanceBook.java
+++ src/main/java/attendance/AttendanceBook.java
@@ -1,10 +1,18 @@
 package attendance;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AttendanceBook {
 
+  private final Map<Crew, AttendanceRecords> attendanceBook = new HashMap<>();
+
   public AttendanceRecord checkAttendance (String nickname, LocalDateTime dateTime) {
-    return null;
+    Crew crew = new Crew(nickname);
+    if(!attendanceBook.containsKey(crew)) {
+      throw new IllegalArgumentException("등록되지 않은 크루입니다");
+    }
+    return attendanceBook.get(crew).save(new AttendanceRecord(dateTime));
   }
 }
