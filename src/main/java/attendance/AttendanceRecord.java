5e182dfcc3a74c38fb85b5356fa29b175056731e
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-01 11:01:02 +0900

    feat(green): AttendanceRecord 생성 기능 구현

--- src/main/java/attendance/AttendanceRecord.java
+++ src/main/java/attendance/AttendanceRecord.java
@@ -4,14 +4,19 @@ import java.time.LocalDateTime;
 
 public class AttendanceRecord {
 
+  private final Crew crew;
+  private final TimeLog timeLog;
+
   public AttendanceRecord(String nickname, LocalDateTime attendanceTime) {
+    this.crew = new Crew(nickname);
+    this.timeLog = new TimeLog(attendanceTime);
   }
 
   public Crew getNickname() {
-    return null;
+    return crew;
   }
 
   public TimeLog getTimeLog() {
-    return null;
+    return timeLog;
   }
 }
