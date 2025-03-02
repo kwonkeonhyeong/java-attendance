d62b3c2fbffa927b45c9431e2ce44e136f0e0d97
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-02 11:21:20 +0900

    fix: 메서드 명 수정

--- src/main/java/attendance/AttendanceRecords.java
+++ src/main/java/attendance/AttendanceRecords.java
@@ -8,12 +8,12 @@ public class AttendanceRecords {
   private final List<AttendanceRecord> attendanceRecords = new ArrayList<>();
 
   public AttendanceRecord save(AttendanceRecord attendanceRecord) {
-    validateExistsAttendanceRecord(attendanceRecord);
+    validateExistentAttendanceRecord(attendanceRecord);
     attendanceRecords.add(attendanceRecord);
     return attendanceRecord;
   }
 
-  private void validateExistsAttendanceRecord(AttendanceRecord attendanceRecord) {
+  private void validateExistentAttendanceRecord(AttendanceRecord attendanceRecord) {
     if(attendanceRecords.contains(attendanceRecord)) {
       throw new IllegalArgumentException("해당 일 출석 기록이 이미 존재합니다");
     }
