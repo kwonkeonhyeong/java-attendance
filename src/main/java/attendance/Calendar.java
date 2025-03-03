a835d9458fb8b31c810ead58146697b8b88b797b
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-03 09:53:18 +0900

    test(red): calendar 이용 가능한 날짜 조회 관련 테스트 추가

---
+++ src/main/java/attendance/Calendar.java
@@ -0,0 +1,12 @@
+package attendance;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public enum Calendar {
+  ;
+
+  public static List<LocalDate> getAvailableDates(LocalDate searchDate) {
+    return null;
+  }
+}
