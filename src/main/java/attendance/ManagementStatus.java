d86588df86c7ce129b8a6c714cf848d95b662f61
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-03-03 13:06:23 +0900

    test(red): 제적 위험자 확인을 위한 계산 기능 테스트 추가

---
+++ src/main/java/attendance/ManagementStatus.java
@@ -0,0 +1,9 @@
+package attendance;
+
+public enum ManagementStatus {
+  EXPLUSION, INTERVIEW, WARNING, GENERAL;
+
+  public static ManagementStatus from(int absenceCont) {
+    return null;
+  }
+}
